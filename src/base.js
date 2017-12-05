bb4c89f71b965bf28a0a0ab07e686aa5103a953f
Darek Tatum <jdtatum@gmail.com>
2017-12-05 09:36:23 -0600

    Initial Commit - Animate

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyB5LZvfJyMdoty5es4BmZGh65A4Z1xv1l0",
+  authDomain: "cath-of-the-day-jdt.firebaseapp.com",
+  databaseURL: "https://cath-of-the-day-jdt.firebaseio.com",
+});
+
+export default base;
