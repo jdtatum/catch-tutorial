dc2272ce4cd81f18dfbe7d9b7739335705ed9d5d
Darek Tatum <jdtatum@gmail.com>
2017-12-06 14:19:44 -0600

    Added bind(this) alternative

--- src/components/App.js
+++ src/components/App.js
@@ -10,13 +10,6 @@ class App extends React.Component {
   constructor() {
     super();
 
-    this.addFish = this.addFish.bind(this);
-    this.removeFish = this.removeFish.bind(this);
-    this.updateFish = this.updateFish.bind(this);
-    this.loadSamples = this.loadSamples.bind(this);
-    this.addToOrder = this.addToOrder.bind(this);
-    this.removeFromOrder = this.removeFromOrder.bind(this);
-
     // getinitialState
     this.state = {
       fishes: {},
@@ -51,7 +44,7 @@ class App extends React.Component {
     localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order));
   }
 
-  addFish(fish) {
+  addFish = (fish) => {
     // update our state
     const fishes = {...this.state.fishes};
     // add in our new fish
@@ -59,40 +52,40 @@ class App extends React.Component {
     fishes[`fish-${timestamp}`] = fish;
     // set state
     this.setState({ fishes });
-  }
+  };
 
-  updateFish(key, updatedFish) {
+  updateFish = (key, updatedFish) => {
     const fishes = {...this.state.fishes};
     fishes[key] = updatedFish;
     this.setState({ fishes });
-  }
+  };
 
-  removeFish(key) {
+  removeFish = (key) => {
     const fishes = {...this.state.fishes};
     fishes[key] = null;
     this.setState({ fishes });
-  }
+  };
 
-  loadSamples() {
+  loadSamples = () => {
     this.setState({
       fishes: sampleFishes
     });
-  }
+  };
 
-  addToOrder(key) {
+  addToOrder = (key) => {
     // take a copy of our state
     const order = {...this.state.order};
     // update or add the new number of fish ordered
     order[key] = order[key] + 1 || 1;
     // update our state
     this.setState({ order });
-  }
+  };
 
-  removeFromOrder(key) {
+  removeFromOrder = (key) => {
     const order = {...this.state.order};
     delete order[key];
     this.setState({ order });
-  }
+  };
 
   render() {
     return (
